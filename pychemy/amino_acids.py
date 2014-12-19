3ea30f562a2060ee1f846b3fdadf578a299b2873
Benjie Chen <benjie@ginkgobioworks.com>
2014-12-18 21:04:12 -0500

    Add amino acid and peptide tools from Tim

---
+++ pychemy/amino_acids.py
@@ -0,0 +1,124 @@
+from pychemy.molmass import Formula
+from pychemy.elements import ELEMENTS, Isotope
+
+
+class Amino_Acid(object):
+  def __init__(self, residue = '', mod = '', formula = '', losses = []):
+    self.residue = residue
+    self.mod = mod
+    self.formula = formula
+    if formula:
+      self.mass = Formula(formula).isotope.mass
+    else:
+      self.mass = 0.0
+    self.losses = losses
+    AMINO_ACIDS.add(self)
+
+  def __str__(self):
+    return self.mod
+
+
+class Loss(object):
+  def __init__(self, name, formula):
+    self.display_name = name
+    self.formula = formula
+    self.mass = Formula(formula).isotope.mass
+    LOSSES.add(self)
+
+    def __str__(self):
+      return self.formula
+
+
+class Amino_Acid_Registry(object):
+  def __init__(self):
+    self._list = []
+    self._dict = {}
+    
+  def __add__(self, amino_acid):
+    if amino_acid.mod in self._dict.keys():
+      raise KeyError
+    else:
+      self._list.append(amino_acid)
+      self._dict[amino_acid.mod] = amino_acid
+  
+  def __str__(self):
+    return "[%s]" % ", ".join(aa.mod for aa in self._list)
+    
+  def __iter__(self):
+    return iter(self._list)
+    
+  def __len__(self):
+    return len(self._list)
+
+  def __getitem__(self, key):
+    return self._dict[key]
+  
+  def add(self, amino_acid):
+    self.__add__(amino_acid)  
+
+
+class Loss_Registry(object):
+  def __init__(self):
+    self._list = []
+    self._dict = {}
+  
+  def __add__(self, loss):
+    if loss.formula in self._dict.keys():
+      raise KeyError
+    else:
+      self._list.append(loss)
+      self._dict[loss.formula] = loss
+  
+  def __str__(self):
+    return "[%s]" % ", ".join(loss.formula for loss in self._list)
+
+  def __iter__(self):
+    return iter(self._list)
+
+  def __len__(self):
+    return len(self._list)
+
+  def __getitem__(self, key):
+    try:
+      return self._dict[key]
+    except:
+      raise KeyError
+
+  def add(self, loss):
+    self.__add__(loss)
+
+
+AMINO_ACIDS = Amino_Acid_Registry()
+LOSSES = Loss_Registry()
+
+
+Loss(name = 'H_2O', formula = 'OH2')
+Loss(name = 'NH_3', formula = 'NH3')
+Loss(name = 'CO', formula = 'CO')
+
+
+Amino_Acid(residue = 'A', mod = 'A', formula = 'C3H5N1O1')
+Amino_Acid(residue = 'R', mod = 'R', formula = 'C6H12N4O1')
+Amino_Acid(residue = 'N', mod = 'N', formula = 'C4H6N2O2')
+Amino_Acid(residue = 'D', mod = 'D', formula = 'C4H5N1O3')
+Amino_Acid(residue = 'C', mod = 'C', formula = 'C3H5N1O1S1')
+Amino_Acid(residue = 'C', mod = 'C(carbamidomethyl)', formula = 'C5H8N2O2S1')
+Amino_Acid(residue = 'Q', mod = 'Q', formula = 'C5H8N2O2')
+Amino_Acid(residue = 'E', mod = 'E', formula = 'C5H7N1O3')
+Amino_Acid(residue = 'G', mod = 'G', formula = 'C2H3N1O1')
+Amino_Acid(residue = 'H', mod = 'H', formula = 'C6H7N3O1')
+Amino_Acid(residue = 'I', mod = 'I', formula = 'C6H11N1O1')
+Amino_Acid(residue = 'L', mod = 'L', formula = 'C6H11N1O1')
+Amino_Acid(residue = 'K', mod = 'K', formula = 'C6H12N2O1')
+Amino_Acid(residue = 'M', mod = 'M', formula = 'C5H9N1O1S1')
+Amino_Acid(residue = 'M', mod = 'M(ox)', formula = 'C5H9N1O2S1')
+
+Amino_Acid(residue = 'F', mod = 'F', formula = 'C9H9N1O1')
+Amino_Acid(residue = 'P', mod = 'P', formula = 'C5H7N1O1')
+Amino_Acid(residue = 'S', mod = 'S', formula = 'C3H5N1O2', losses = [LOSSES['OH2']])
+Amino_Acid(residue = 'T', mod = 'T', formula = 'C4H7N1O2', losses = [LOSSES['OH2']])
+Amino_Acid(residue = 'W', mod = 'W', formula = 'C11H10N2O1')
+Amino_Acid(residue = 'Y', mod = 'Y', formula = 'C9H9N1O2')
+Amino_Acid(residue = 'V', mod = 'V', formula = 'C5H9N1O1')
+Amino_Acid(residue = 'N-term', mod = 'N-term', formula = '')
+Amino_Acid(residue = 'C-term', mod = 'C-term', formula = 'OH2')
---
+++ pychemy/peptides.py
@@ -0,0 +1,184 @@
+from pychemy.amino_acids import AMINO_ACIDS, LOSSES
+from pychemy.molmass import Formula
+import re
+
+
+# Parse string containing sequence into residues
+def residues_from_sequence(sequence = ''):
+  residues = re.findall('[A-Z](?:\([^\)]+\))?', sequence)
+
+  for item in residues:
+    sequence = re.sub(re.sub('\)', '\\\)', re.sub('\(', '\\\(', item)), '', sequence)
+
+  if len(sequence) > 0:
+    raise Exception('Sequence string contains improperly formatted residue: ' + str(sequence))
+  else:
+    return residues
+
+
+# Calculate mass of peptide based on chemical formula entered as 'ACDE'
+def mass_from_sequence(sequence = '', nterm = 'N-term', cterm = 'C-term'):
+  seq_residues = residues_from_sequence(sequence)
+  mass = 0
+
+  # Include N-terminal group mass
+  mass += AMINO_ACIDS[nterm].mass
+
+  # Include each residue mass
+  for r in seq_residues:
+    mass += AMINO_ACIDS[r].mass
+      
+  # Include C-terminal group mass
+  mass += AMINO_ACIDS[cterm].mass
+
+  return mass
+
+
+def update_loss(loss, new_loss):
+  for idx,l in enumerate(loss):
+    if l[1].formula == LOSSES[new_loss].formula:
+      loss[idx][0] += 1
+      return loss
+  return loss + [[1, LOSSES[new_loss]]]
+
+
+def loss_name(loss = []):
+  out = ''
+  for l in loss:
+    if l[0] == 1:
+      out += ' - ' + l[1].display_name
+    else:
+      out += ' - ' + str(l[0]) + ' ' + l[1].display_name
+  return out
+
+
+def predict_positive_fragments_from_sequence(sequence = [], nterm = 'N-term', cterm = 'C-term',
+                                             max_charge_state = 1):
+  out = []
+
+  fragments = predict_fragments_from_sequence(sequence = sequence, cterm = cterm)
+
+  for item in fragments:
+    out.append(( round(item[0] + Formula('H').isotope.mass, 5) , item[1] + ' (+1)'))
+    
+    for cs in range(2,max_charge_state + 1):
+      if item[0] > 200 * cs:
+        out.append(( round((item[0] + cs * Formula('H').isotope.mass) / cs, 5), item[1] + ' (+' +  str(cs) + ')'))
+  
+  out = sorted(list(set(out)), key = lambda x:x[0])
+  return out
+
+
+def predict_negative_fragments_from_sequence(sequence = [], nterm = 'N-term', cterm = 'C-term',
+                                             max_charge_state = 1):
+  out = []
+
+  fragments = predict_fragments_from_sequence(sequence = sequence, cterm = cterm)
+
+  for item in fragments:
+    out.append(( round(item[0] - Formula('H').isotope.mass, 5), item[1] + ' (-1)'))
+    
+    for cs in range(2,max_charge_state + 1):
+      if item[0] > 200 * cs:
+        out.append(( round((item[0] - cs * Formula('H').isotope.mass) / cs, 5), item[1] + ' (-' +  str(cs) + ')'))
+  
+  out = sorted(list(set(out)), key = lambda x:x[0])
+  return out
+
+
+# Predict fragments from peptide sequence    
+def predict_fragments_from_sequence(sequence = [], nterm = 'N-term', cterm = 'C-term'):
+  out = []
+
+  # b-ions
+  nterm_mass = AMINO_ACIDS[nterm].mass
+  out += append_residue(mass = nterm_mass, 
+                        pos = 1, 
+                        ion_type = 'b_', 
+                        seq = sequence, 
+                        term = cterm, 
+                        loss = [])
+  for l in AMINO_ACIDS[nterm].losses:
+    out += append_residue(mass = nterm_mass - l.mass, 
+                          pos = 1, 
+                          ion_type = 'b_', 
+                          seq = sequence, 
+                          term = cterm, 
+                          loss = update_loss([],l.formula))
+
+  # a-ions
+  nterm_mass = AMINO_ACIDS[nterm].mass - Formula('CO').isotope.mass 
+  out += append_residue(mass = nterm_mass, 
+                        pos = 1, 
+                        ion_type = 'a_', 
+                        seq = sequence, 
+                        term = cterm, 
+                        loss = [])
+  for l in AMINO_ACIDS[nterm].losses:
+    out += append_residue(mass = nterm_mass - l.mass, 
+                          pos = 1, 
+                          ion_type = 'a_', 
+                          seq = sequence, 
+                          term = cterm, 
+                          loss = update_loss([],l.formula))
+
+  # y-ions
+  cterm_mass = AMINO_ACIDS[cterm].mass
+  out += append_residue(mass = cterm_mass, 
+                        pos = 1, 
+                        ion_type = 'y_', 
+                        seq = sequence[::-1], 
+                        term = nterm, 
+                        loss = [])
+  for l in AMINO_ACIDS[cterm].losses:
+    out += append_residue(mass = cterm_mass - l.mass, 
+                          pos = 1, 
+                          ion_type = 'y_', 
+                          seq = sequence[::-1], 
+                          term = nterm, 
+                          loss = update_loss([],l.formula))
+      
+  out = sorted(list(set(out)), key = lambda x:x[0])
+  return out
+
+
+def append_residue(mass, pos, ion_type = '', seq = [], term = '', loss = []):
+  out = []
+
+  if pos == 1 and not seq:
+    raise Exception("append_residue encountered empty sequence")
+
+  if len(seq) == 0:
+    # No residues left to place, place terminus
+    r = AMINO_ACIDS[term]
+
+    if ion_type ==  'a_':
+      # Special case: M - CO
+      loss = update_loss(loss, 'CO')  
+    
+    out += [(round(mass + r.mass, 5), 'M' + loss_name(loss))]
+    for l in r.losses: 
+      out += [(round(mass + r.mass - l.mass, 5), 'M' + loss_name(update_loss(loss, l.formula)))]
+
+  else:
+    # Place each loss form of next residue
+    r = AMINO_ACIDS[seq[0]]
+
+    out += [(round(mass + r.mass, 5), ion_type + str(pos) + loss_name(loss))]          
+    out += append_residue(mass = mass + r.mass, 
+                          pos = pos + 1, 
+                          ion_type = ion_type, 
+                          seq = seq[1:], 
+                          term = term, 
+                          loss = loss)
+    for l in r.losses:
+      curr_loss = update_loss(loss, l.formula)
+      out += [(round(mass + r.mass - l.mass, 5), ion_type + str(pos) + loss_name(curr_loss))]
+      out += append_residue(mass = mass + r.mass - l.mass, 
+                            pos = pos + 1, 
+                            ion_type = ion_type, 
+                            seq = seq[1:], 
+                            term = term, 
+                            loss = curr_loss)
+
+  return out
